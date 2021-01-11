ebe85ab2f84371882e415f8b005f1ea084f10514
jcosca <jardel.cancado@gmail.com>
2021-01-11 07:57:27 -0300

    Colocar em html

--- arrays.php
+++ arrays.php
@@ -1,6 +1,27 @@
-<? php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    <?php 
 
-$array1 = ['ES', 'MG', 'RJ', 'SP'];
-$array2 = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo'];
+    $array1 = ['ES', 'MG', 'RJ', 'SP'];
+    $array2 = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo'];
+    $array3 = [];
 
-?>
+    foreach ($array1 as $key => $value) {
+        $array3[$value] = $array2[3 - $key]
+    }
+
+    foreach ($array3 as $key => $value) {
+        echo $key." - ".$value.PHP_EOL;
+    }
+
+    ?>
+
+</body>
+</html>
+
