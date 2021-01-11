5baef58a0d61dd520d583b21768fe4636b1b3d33
jcosca <jardel.cancado@gmail.com>
2021-01-11 08:32:24 -0300

    CCA

--- arrays.php
+++ arrays.php
@@ -6,21 +6,21 @@
     <title>Arrays</title>
 </head>
 <body>
-    <?php 
+<?php 
 
     $array1 = ['ES', 'MG', 'RJ', 'SP'];
     $array2 = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo'];
     $array3 = [];
 
     foreach ($array1 as $key => $value) {
-        $array3[$value] = $array2[3 - $key]
+        $array3[$value] = $array2[(3 - $key)];
     }
 
     foreach ($array3 as $key => $value) {
         echo $key." - ".$value.PHP_EOL;
     }
 
-    ?>
+?>
 
 </body>
 </html>
